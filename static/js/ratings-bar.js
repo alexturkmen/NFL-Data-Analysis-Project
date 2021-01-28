832b2a3f24de62c2cec3414c126d70706bb977b5
anisali01 <anis.ali01@gmail.com>
2021-01-27 23:37:07 -0500

    neilson rating data added to graphs.html

--- static/js/ratings-bar.js
+++ static/js/ratings-bar.js
@@ -1,59 +1,89 @@
-console.log("Hello")
+(function (d3) {
+  "use strict";
 
-var svg = d3.select("svg"),
-  margin = 250,
-  width = svg.attr("width") - margin,
-  height = svg.attr("height") - margin;
+  const titleText = "Neilson Rating for NFL Superbowl Games";
+  const xAxisLabelText = "Neilson Rating";
 
-var xScale = d3.scaleBand().range([0, width]).padding(0.4),
-  yScale = d3.scaleLinear().range([height, 0]);
+  const svg = d3.select("svg");
 
-var g = svg.append("g").attr("transform", "translate(" + 100 + "," + 100 + ")");
+  const width = +svg.attr("width");
+  const height = +svg.attr("height");
 
-d3.csv("../static/js/ratings.csv").then(data=>{
-  // if (error) {
-  //   throw error;
-  // }
+  const render = (data) => {
+    const xValue = (d) => d["NeilsonRating"];
+    const yValue = (d) => d.Year;
+    const margin = { top: 50, right: 40, bottom: 77, left: 180 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
-  xScale.domain(
-    data.map(function (d) {
-      return d.Year;
-    })
-  );
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, xValue)])
+      .range([0, innerWidth]);
 
-  yScale.domain([
-    0,
-    d3.max(data, function (d) {
-      return d.NeilsonRating;
-    }),
-  ]);
+    const yScale = d3
+      .scaleBand()
+      .domain(data.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.1);
 
-  g.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale));
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  g.append("g").call(
-    d3
-      .axisLeft(yScale)
-      .tickFormat(function (d) {
-        return d;
-      })
-      .ticks(10)
-  );
+    const xAxisTickFormat = (number) =>
+      d3.format(".3s")(number).replace("G", "B");
 
-  g.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("class", "bar")
-    .attr("x", function (d) {
-      return xScale(d.Year);
-    })
-    .attr("y", function (d) {
-      return yScale(d.NeilsonRating);
-    })
-    .attr("width", xScale.bandwidth())
-    .attr("height", function (d) {
-      return height - yScale(d.NeilsonRating);
+    const xAxis = d3
+      .axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+
+    g.append("g")
+      .call(d3.axisLeft(yScale))
+      .selectAll(".domain, .tick line")
+      .style("font-size", "30px")
+      .remove();
+
+    const xAxisG = g
+      .append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0,${innerHeight})`)
+      .style("font-size", "19px");
+
+    xAxisG.select(".domain").remove();
+
+    xAxisG
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("y", 65)
+      .attr("x", innerWidth / 2)
+      .attr("fill", "black")
+      .text(xAxisLabelText)
+      .style("font-size", "30px");
+
+    g.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y", (d) => yScale(yValue(d)))
+      .attr("width", (d) => xScale(xValue(d)))
+      .attr("height", yScale.bandwidth())
+      .style("font-size", "10px");
+
+    g.append("text")
+      .attr("class", "title")
+      .attr("y", -10)
+      .text(titleText)
+      .style("font-size", "30px");
+  };
+
+  d3.csv("../static/csv/ratings.csv").then((data) => {
+    data.forEach((d) => {
+      d.NeilsonRating = d.NeilsonRating;
+      d.Year = d.Year;
+      console.log(d.Year, d.NeilsonRating);
     });
-}).catch(err=>console.log(err));
+    render(data);
+  });
+})(d3);
